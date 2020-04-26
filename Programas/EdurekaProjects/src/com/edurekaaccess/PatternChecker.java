645cb8fab77209968ec879a153d356b3597f5204
Guilherme Vinícius Amorim <guilherme.vini65@gmail.com>
2020-04-26 18:12:52 -0300

    Update PatternChecker.java

--- Programas/EdurekaProjects/src/com/edurekaaccess/PatternChecker.java
+++ Programas/EdurekaProjects/src/com/edurekaaccess/PatternChecker.java
@@ -6,13 +6,32 @@ import java.util.regex.Pattern;
 public class PatternChecker {
 
 	public static void main(String[] args) {
-		String pattern = "[a-z]+";
-		String stringToCheck = "Happy Learning!!";
-		Pattern compiledPattern = Pattern.compile(pattern);
-		Matcher matcher = compiledPattern.matcher(stringToCheck);
-		while(matcher.find()) {
-			System.out.println(stringToCheck.substring(matcher.start(), matcher.end()));
+		
+		String stringToCheck = "Happy Learning in Edureka!!";
+		
+		String pattern1 = "[a-z]+";
+		String pattern2 = "in";
+		String pattern3 = "\\bin\\b";
+		
+		Pattern compiledPattern1 = Pattern.compile(pattern1);
+		Pattern compiledPattern2 = Pattern.compile(pattern2);
+		Pattern compiledPattern3 = Pattern.compile(pattern3);
+		
+		Matcher matcher1 = compiledPattern1.matcher(stringToCheck);
+		Matcher matcher2 = compiledPattern2.matcher(stringToCheck);
+		Matcher matcher3 = compiledPattern3.matcher(stringToCheck);
+		
+		
+		while(matcher1.find()) {
+			System.out.println("Pattern1 matches: " + stringToCheck.substring(matcher1.start(), matcher1.end()));
 		}
+		while(matcher2.find()) {
+			System.out.println("Pattern2 matches: letters " + matcher2.group() + " at " + matcher2.start());
+		}
+		while(matcher3.find()) {
+			System.out.println("Pattern3 matches: word " + matcher3.group() + " at " + matcher3.start());
+		}
+		
 		
 	}
 	
